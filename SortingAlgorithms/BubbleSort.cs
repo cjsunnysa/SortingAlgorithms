3cb71600abea568538fd3c682d0faf3e17e02965
Chris Jones <cjsunnysa@hotmail.com>
2017-10-27 06:09:30 +0200

    create: bubble sort ascending.

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public static class BubbleSort
+    {
+        public static int[] SortAscending(int[] array)
+        {
+            for (int i = array.Length - 1; i >= 1; --i)
+                for (int j = 1; j <= i - 1; ++j)
+                    if (array[j] > array[j+1])
+                    {
+                        var temp = array[j+1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+
+            return array;
+        }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,54 @@ namespace SortingAlgorithms
 {
     class Program
     {
+        static Random _randomGenerator = new Random();
+        static Stopwatch _stopwatch = new Stopwatch();
+
         static void Main(string[] args)
         {
+            var unsortedArray = GenerateSet(100000).ToArray();
+
+            _stopwatch.Start();
+            var sortedArray = BubbleSort.SortAscending(unsortedArray);
+            _stopwatch.Stop();
+
+            //PrintArray("unsorted", unsortedArray);
+            //PrintArray("sorted", sortedArray);
+
+            Console.WriteLine($"elapsed time: {_stopwatch.ElapsedMilliseconds/1000d} seconds");
+
+            Console.ReadKey();
+        }
+
+        private static HashSet<int> GenerateSet(int totalRecords)
+        {
+            var set = new HashSet<int>();
+
+            while(set.Count < totalRecords)
+            {
+                var newInt = _randomGenerator.Next(1, totalRecords * 2);
+
+                if (set.Contains(newInt))
+                    continue;
+
+                set.Add(newInt);
+            }
+
+            return set;
+        }
+
+        private static void PrintArray(string title, int[] array)
+        {
+            Console.WriteLine(title + ":");
+
+            foreach (var num in array)
+            {
+                var ending = num == array[array.Length - 1] ? "\n" : ", ";
+
+                Console.Write($"{num}{ending}");
+            }
+
+            Console.WriteLine();
         }
     }
 }
